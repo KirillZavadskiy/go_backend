c66dc4b2d244f65ccd53f7cdb2141834f1be3917
Кирилл Завадский <zavad93@mail.ru>
2024-03-11 14:13:12 +0300

    Add file structMethod.

---
+++ structMethod.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+type User struct {
+	name  string
+	age   int16
+	score []int
+}
+
+func changeAge(u *User) {
+	u.age = 23
+}
+func (u *User) setName(name1 string) string {
+	u.name = name1
+	return u.name
+}
+func (u User) getScore() int {
+	max := 0
+	for _, sc := range u.score {
+		if max < sc {
+			max = sc
+		}
+	}
+	return max
+}
+func main() {
+	u := User{"Kirill", 30, []int{1, 2, 3, 97, 5, 6}}
+	changeAge(&u)
+	u.setName("Den")
+	fmt.Println(&u)
+	fmt.Printf("Максимальный счет =%d", u.getScore())
+}
