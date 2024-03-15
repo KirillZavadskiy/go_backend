41ed1aeeb7657b385bc36b91ade2b623fecc7132
Кирилл Завадский <zavad93@mail.ru>
2024-03-15 15:59:41 +0300

    :monocle_face:

--- calculator/main.go
+++ calculator/main.go
@@ -14,16 +14,23 @@ func main() {
 	a := app.New()
 	w := a.NewWindow("Мини калькулятор")
 	w.Resize(fyne.NewSize(300, 400))
+	icon, err := fyne.LoadResourceFromPath("icon.png")
+	if err != nil {
+		fmt.Println("Лого не загрузилось")
+	}
+	w.SetIcon(icon)
 
 	label1 := widget.NewLabel("Введите первое число:")
 	entry1 := widget.NewEntry()
+	entry1.SetPlaceHolder("Первое число")
 
-	label2 := widget.NewLabel("Введите знак")
+	label2 := widget.NewLabel("Выберите знак:")
 	entry2 := widget.NewRadioGroup([]string{"+", "-", "*", "/"}, func(string) {})
 	entry2.Horizontal = true
 
 	label3 := widget.NewLabel("Введите второе число:")
 	entry3 := widget.NewEntry()
+	entry3.SetPlaceHolder("Второе число")
 
 	answer := widget.NewLabel("")
 	button := widget.NewButton("Вычислить", func() {
@@ -44,6 +51,8 @@ func main() {
 		} else if entry2.Selected == "/" {
 			div := n1 / n3
 			answer.SetText(fmt.Sprintf("Деление = %f", div))
+		} else {
+			answer.SetText("Ошибка! Выберите знак")
 		}
 	})
 
